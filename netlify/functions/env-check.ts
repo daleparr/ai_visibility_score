8181fdcd4b2c86211b1fa66eca0869a221231fea
Your Name <you@yourcompany.com>
2025-09-19 15:34:21 +0100

    feat: add env-check function

---
+++ netlify/functions/env-check.ts
@@ -0,0 +1,10 @@
+﻿export async function handler() {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      DATABASE_URL: !!process.env.DATABASE_URL,
+      NETLIFY_DATABASE_URL: !!process.env.NETLIFY_DATABASE_URL,
+      NETLIFY_DATABASE_URL_UNPOOLED: !!process.env.NETLIFY_DATABASE_URL_UNPOOLED
+    })
+  };
+}
